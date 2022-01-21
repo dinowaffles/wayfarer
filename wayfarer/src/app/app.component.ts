6e95953db44929e4c6d8cf5452cc870408695f30
Abby Henes <abbyhenes@gmail.com>
2022-01-20 22:02:15 -0600

    got hero image to only display on landing

--- wayfarer/src/app/app.component.ts
+++ wayfarer/src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
-import { CITIES } from './homepage/cities'
+import { Component, OnInit } from '@angular/core';
+import { CITIES } from './homepage/cities';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+
+
+export class AppComponent implements OnInit {
+
+  constructor(public router: Router) {}
+
+  ngOnInit() {}
+
   title = 'wayfarer';
   cities = CITIES;
 }
