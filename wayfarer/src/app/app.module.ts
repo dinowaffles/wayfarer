0d3ede3dcb0f648d5d513e64e288aa9832756002
Avantika <singh_avantika@yahoo.com>
2022-01-21 11:16:28 -0600

    Homepage component edit

--- wayfarer/src/app/app.module.ts
+++ wayfarer/src/app/app.module.ts
@@ -5,16 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-<<<<<<< HEAD
 
-import { SidebarComponent } from './homepage/sidebar/sidebar.component';
-import { CityDetailsComponent } from './homepage/city-details/city-details.component';
 
-=======
 import { SidebarComponent } from './../app/homepage/sidebar/sidebar.component';
 import { CityDetailsComponent } from './../app/homepage/city-details/city-details.component';
 import { HomepageComponent } from './homepage/homepage/homepage.component';
->>>>>>> 0d6496fa9d0a466647829a869749579d0eab0099
+
 
 @NgModule({
   declarations: [
