396af19692fd0a746da4b6f710d309268729aea2
Abby Henes <abbyhenes@gmail.com>
2022-01-21 10:41:06 -0600

    added posts div to homepage

--- wayfarer/src/app/homepage/cities.ts
+++ wayfarer/src/app/homepage/cities.ts
@@ -4,11 +4,17 @@ export const CITIES = [
         name: 'Chicago',
         photo: 'assets/images/chicago-skyline',
         posts: [{
-            title: 'chicago title',
+            title: 'chicago post1',
             date: '01-20-2022', 
             author: 'Avantika',
             text: 'lorem ipsem'
-        }
+        },
+        {
+            title: 'chicago post2',
+            date: '01-22-2022', 
+            author: 'Abby',
+            text: 'lorem ipsem'
+        },
         ],
     },
     {
@@ -16,12 +22,18 @@ export const CITIES = [
         name: 'Lincoln',
         photo: 'assets/images/lincolnne',
         posts: [{
-            title: 'lincoln title',
+            title: 'lincoln post1',
             date: '01-20-2022', 
             author: 'Abby',
             text: 'lorem ipsem'
-        }
-        ]
+        },
+        {
+            title: 'lincoln post2',
+            date: '01-22-2022', 
+            author: 'Avantika',
+            text: 'lorem ipsem'
+        },
+        ],
     }
 
 ]
