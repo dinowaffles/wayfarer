fcf5dbf6c60ac4216a21964005ccce6a1037770c
Avantika <singh_avantika@yahoo.com>
2022-01-21 10:30:29 -0600

    cities data edit

--- wayfarer/src/app/homepage/cities.ts
+++ wayfarer/src/app/homepage/cities.ts
@@ -1,23 +1,23 @@
 export const CITIES = [
     {
-        id: 1, 
+        id: 1,
         name: 'Chicago',
-        photo: 'assets/images/chicago-skyline',
+        photo: 'assets/images/chicago-skyline.jpeg',
         posts: [{
             title: 'chicago title',
-            date: '01-20-2022', 
+            date: '01-20-2022',
             author: 'Avantika',
             text: 'lorem ipsem'
         }
         ],
     },
     {
-        id: 2, 
+        id: 2,
         name: 'Lincoln',
-        photo: 'assets/images/lincolnne',
+        photo: 'assets/images/lincolnne.jpeg',
         posts: [{
             title: 'lincoln title',
-            date: '01-20-2022', 
+            date: '01-20-2022',
             author: 'Abby',
             text: 'lorem ipsem'
         }
