58f1808205ef53d94805709eb5db6b22f96c1f79
Abby Henes <abbyhenes@gmail.com>
2022-01-22 15:56:09 -0600

    got dropdown menu to work

--- wayfarer/src/app/app.module.ts
+++ wayfarer/src/app/app.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
@@ -13,6 +14,7 @@ import { CityDetailsComponent } from './../app/homepage/city-details/city-detail
 import { HomepageComponent } from './homepage/homepage/homepage.component';
 import { PostsComponent } from './homepage/posts/posts.component';
 import { SearchPipe } from './search.pipe';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
@@ -29,7 +31,10 @@ import { SearchPipe } from './search.pipe';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
